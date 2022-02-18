4884b05ae296297ca55b285cb067fb2fc38eeff0
Darwin Baker <51175243+DarwinBaker@users.noreply.github.com>
2022-02-17 19:21:52 -0500

    Netcode update for future web tracker

---
+++ AATool/Net/AAKey.cs
@@ -0,0 +1,13 @@
+﻿
+namespace AATool.Net
+{
+    public static class AAKey
+    {
+        public const string RandomCharacters = "0123456789";
+        public const string Prefix = "AAKEY-";
+        public const int Length = 16;
+
+        public static string Strip(string fullKey) 
+            => fullKey?.Replace(Prefix, "").Replace("-", "");
+    }
+}
---
+++ AATool/Net/OpenTracker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Http;
+using AATool.Configuration;
+using AATool.Utilities;
+
+namespace AATool.Net
+{
+    static class OpenTracker
+    {
+        public static Exception LastError { get; private set; }
+        public static string WatchUrl { get; private set; }
+
+        private static readonly HttpClient Client = new ();
+        private static readonly Timer Cooldown = new (5);
+
+        private static bool Invalidated = false;
+
+        public static void Update(Time time)
+        {
+            Cooldown.Update(time);
+            if (Invalidated && !IsOnCooldown)
+                BroadcastProgress();
+        }
+
+        public static bool IsOnCooldown => Cooldown.IsRunning;
+
+        public static async void BroadcastProgress()
+        {
+            if (IsOnCooldown)
+            {
+                Invalidated = true;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Config.Tracking.OpenTrackerKey))
+                return;
+            if (string.IsNullOrEmpty(Config.Tracking.OpenTrackerUrl))
+                return;
+
+            Cooldown.Reset();
+            Invalidated = false;
+
+            try
+            {
+                string aaKey = AAKey.Strip(Config.Tracking.OpenTrackerKey);
+                var content = new StringContent(aaKey + Tracker.State.ToJsonString());
+                HttpResponseMessage response = await Client.PostAsync(Config.Tracking.OpenTrackerUrl, content);
+                string message = await response.Content.ReadAsStringAsync();
+                WatchUrl = message;
+                LastError = null;
+            }
+            catch (Exception e) { LastError = e; }
+        }
+    }
+}
