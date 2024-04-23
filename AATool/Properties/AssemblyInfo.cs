42307cc08f17860dce07a5e077e51dafb609d41a
DarwinBaker <Darwin.Baker705@gmail.com>
2024-04-23 17:11:33 -0400

    Update version number

--- AATool/Properties/AssemblyInfo.cs
+++ AATool/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.7.2.0")]
-[assembly: AssemblyFileVersion("1.7.2.0")]
+[assembly: AssemblyVersion("1.7.2.1")]
+[assembly: AssemblyFileVersion("1.7.2.1")]
 [assembly: NeutralResourcesLanguage("en")]
